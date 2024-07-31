64f07ee4a8e22ac8d0f71ee71fe751723cf4527c
jiangweiye <jiangwy@staryea.com>
2024-07-31 10:09:14 +0800

    doc: add readme.md

--- packages/yaml/core/src/lib.rs
+++ packages/yaml/core/src/lib.rs
@@ -16,3 +16,30 @@ pub fn dump(data: &JsValue) -> Result<String, JsValue> {
     let obj: Value = from_value(data.clone()).map_err(|e| JsValue::from_str(&e.to_string()))?;
     serde_yaml::to_string(&obj).map_err(|e| JsValue::from_str(&e.to_string()))
 }
+
+// 合并数据
+#[wasm_bindgen]
+pub fn merge(data: &JsValue, patch: &JsValue) -> Result<JsValue, JsValue> {
+    let mut data: Value =
+        from_value(data.clone()).map_err(|e| JsValue::from_str(&e.to_string()))?;
+    let patch: Value = from_value(patch.clone()).map_err(|e| JsValue::from_str(&e.to_string()))?;
+    merge_value(&mut data, &patch);
+    to_value(&data).map_err(|e| JsValue::from_str(&e.to_string()))
+}
+
+fn merge_value(data: &mut Value, patch: &Value) {
+    match (data, patch) {
+        (Value::Object(data), Value::Object(patch)) => {
+            for (key, value) in patch {
+                if let Some(data_value) = data.get_mut(key) {
+                    merge_value(data_value, value);
+                } else {
+                    data.insert(key.clone(), value.clone());
+                }
+            }
+        }
+        (data, patch) => {
+            *data = patch.clone();
+        }
+    }
+}
