80da3fa34412b8e197ff20adcccef9d48a7b6b8e
jiangwy <jiangwy@staryea.com>
2024-08-22 13:46:03 +0800

    fix: wasm build

--- packages/yaml/build/build.ts
+++ packages/yaml/build/build.ts
@@ -1,12 +1,13 @@
 import path from 'node:path';
 import process from 'node:process';
+import fs from 'node:fs';
 import { execa } from 'execa';
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const wasm_exec_dir = path.resolve(__dirname, '../core');
 
 async function build() {
-    await execa('wasm-pack', ['build', '--target', 'bundler', '--out-dir', '../dist'], {
+    await execa('wasm-pack', ['build', '--target', 'web', '--out-dir', '../dist'], {
         cwd: wasm_exec_dir,
         stdio: 'inherit'
     })
@@ -18,6 +19,15 @@ async function build() {
             // 删除输出目录下的package.json文件 .gitignore
             execa('rm', ['-rf', path.resolve(__dirname, '../dist/package.json')]);
             execa('rm', ['-rf', path.resolve(__dirname, '../dist/.gitignore')]);
+
+            const wasm_file = path.resolve(__dirname, '../dist/wasm_yaml.js');
+            const wasm_file_content = fs.readFileSync(wasm_file, 'utf8');
+            const wasm_file_content_replaced = wasm_file_content.replace(
+                `new URL('wasm_yaml_bg.wasm', import.meta.url);`,
+                `await import('./wasm_yaml_bg.wasm').then(m => m.default);`
+            );
+
+            fs.writeFileSync(wasm_file, wasm_file_content_replaced, 'utf8');
         });
 }
 
